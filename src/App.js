a2d8a27979976d9a6cfb8a4f43936fb6cfec6dee
rosbertguevarra <crisiscorefair@gmail.com>
2019-08-09 13:11:36 +0800

    added basic styles in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import weather from "./api/apixu";
+import Banner from "./component/Banner";
 
 export default class App extends Component {
+  state = {};
+
   render() {
     return (
       <div className="container">
-        <h1>Weather App</h1>
+        <Banner />
+        <form className="form-input">
+          <input type="text" placeholder="Search for a City" />
+          <a class="button is-primary">Search</a>
+        </form>
       </div>
     );
   }
---
+++ src/component/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style/home.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Weathering</h1>
+      <p>A website for searching current weather!</p>
+    </div>
+  );
+};
+
+export default Banner;
